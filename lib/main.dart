dc7e3d3683f7a97f801d568693574978ca67a6f9
malak4822 <kmalak4822@gmail.com>
2021-11-10 08:11:26 +0100

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -34,71 +34,106 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Container(
-      color: Colors.black,
-      child: GridView.count(
-        crossAxisCount: 2,
-        crossAxisSpacing: 40.0,
-        mainAxisSpacing: 40.0,
-        children: [
-          Container(
-            child: Center(
-              child: Text(
-                "Let's play 1'st game",
-                textAlign: TextAlign.center,
-                style:
-                    GoogleFonts.overpass(fontSize: 31.0, color: Colors.white),
+      body: Container(
+        color: Colors.black87,
+        child: Stack(
+          children: [
+            SizedBox(
+              width: 180.0,
+              height: 400.0,
+              child: ElevatedButton(
+                onPressed: () {},
+                child: Text(
+                  "Let's play 1'st game",
+                  textAlign: TextAlign.center,
+                  style:
+                      GoogleFonts.overpass(fontSize: 31.0, color: Colors.white),
+                ),
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.redAccent,
+                  side: const BorderSide(width: 3, color: Colors.white),
+                  elevation: 30,
+                  shape: const RoundedRectangleBorder(
+                      borderRadius:
+                          BorderRadius.only(bottomRight: Radius.circular(80))),
+                ),
               ),
             ),
-            decoration: const BoxDecoration(
-              color: Colors.redAccent,
-              borderRadius: BorderRadius.only(bottomRight: Radius.circular(80)),
-            ),
-          ),
-          Container(
-            child: Center(
-              child: Text(
-                "Let's play 2'nd game",
-                textAlign: TextAlign.center,
-                style:
-                    GoogleFonts.overpass(fontSize: 31.0, color: Colors.white),
+            Align(
+              alignment: Alignment.topRight,
+              child: SizedBox(
+                width: 180.0,
+                height: 400.0,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: Text(
+                    "Let's play 1'st game",
+                    textAlign: TextAlign.center,
+                    style: GoogleFonts.overpass(
+                        fontSize: 31.0, color: Colors.white),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.greenAccent,
+                    side: const BorderSide(width: 3, color: Colors.white),
+                    elevation: 30,
+                    shape: const RoundedRectangleBorder(
+                        borderRadius:
+                            BorderRadius.only(bottomLeft: Radius.circular(80))),
+                  ),
+                ),
               ),
             ),
-            decoration: const BoxDecoration(
-              color: Colors.greenAccent,
-              borderRadius: BorderRadius.only(bottomLeft: Radius.circular(80)),
-            ),
-          ),
-          Container(
-            child: Center(
-              child: Text(
-                "Let's read the rules",
-                textAlign: TextAlign.center,
-                style:
-                    GoogleFonts.overpass(fontSize: 31.0, color: Colors.white),
+            Align(
+              alignment: Alignment.bottomLeft,
+              child: SizedBox(
+                width: 180.0,
+                height: 400.0,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: Text(
+                    "Let's play 1'st game",
+                    textAlign: TextAlign.center,
+                    style: GoogleFonts.overpass(
+                        fontSize: 31.0, color: Colors.white),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.blueAccent,
+                    side: const BorderSide(width: 3, color: Colors.white),
+                    elevation: 30,
+                    shape: const RoundedRectangleBorder(
+                        borderRadius:
+                            BorderRadius.only(topRight: Radius.circular(80))),
+                  ),
+                ),
               ),
             ),
-            decoration: const BoxDecoration(
-              color: Colors.lightBlue,
-              borderRadius: BorderRadius.only(topRight: Radius.circular(80)),
-            ),
-          ),
-          Container(
-            child: Center(
-              child: Text(
-                "Let's leave the APK",
-                textAlign: TextAlign.center,
-                style:
-                    GoogleFonts.overpass(fontSize: 31.0, color: Colors.white),
+            Align(
+              alignment: Alignment.bottomRight,
+              child: SizedBox(
+                width: 180.0,
+                height: 400.0,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: Text(
+                    "Let's play 1'st game",
+                    textAlign: TextAlign.center,
+                    style: GoogleFonts.overpass(
+                        fontSize: 31.0, color: Colors.white),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.yellowAccent,
+                    side: const BorderSide(width: 3, color: Colors.white),
+                    elevation: 30,
+                    shape: const RoundedRectangleBorder(
+                        borderRadius:
+                            BorderRadius.only(topLeft: Radius.circular(80))),
+                  ),
+                ),
               ),
             ),
-            decoration: const BoxDecoration(
-              color: Colors.yellowAccent,
-              borderRadius: BorderRadius.only(topLeft: Radius.circular(80)),
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
-    ));
+    );
   }
 }
